640f03fb4c23382b08f72877a5d44f889ce02b79
nmilosev <nmilosevnm@gmail.com>
2016-05-24 03:09:37 +0200

    menu fix

--- PMF/PMF/ViewModels/MenuViewModel.cs
+++ PMF/PMF/ViewModels/MenuViewModel.cs
@@ -54,15 +54,28 @@ namespace PMF.ViewModels
             {
                 return new List<MenuItem>()
                 {
-                    new MenuItem() { Id = "News", Text = Dictionaries.AppDictionary.ButtonNews, Command = new Command<string>(CommandAction), Icon = "newspaper.png" },
-                    new MenuItem() { Id = "Schedule", Text = Dictionaries.AppDictionary.ButtonSchedule, Command = new Command<string>(CommandAction),Icon = "calendar.png" },
-                    new MenuItem() { Id = "Programs", Text = Dictionaries.AppDictionary.ButtonPrograms, Command = new Command<string>(CommandAction), Icon = "arrows.png" },
-                    new MenuItem() { Id = "Contact", Text = Dictionaries.AppDictionary.ButtonContact, Command = new Command<string>(CommandAction), Icon = "phonebook.png" },
-                    new MenuItem() { Id = "StudService", Text = Dictionaries.AppDictionary.ButtonStudServices, Command = new Command<string>(CommandAction), Icon = "sitemap.png" }
+                    new MenuItem() { Id = "News", Text = Dictionaries.AppDictionary.ButtonNews, Icon = "newspaper.png" },
+                    new MenuItem() { Id = "Schedule", Text = Dictionaries.AppDictionary.ButtonSchedule, Icon = "calendar.png" },
+                    new MenuItem() { Id = "Programs", Text = Dictionaries.AppDictionary.ButtonPrograms, Icon = "arrows.png" },
+                    new MenuItem() { Id = "Contact", Text = Dictionaries.AppDictionary.ButtonContact, Icon = "phonebook.png" },
+                    new MenuItem() { Id = "StudService", Text = Dictionaries.AppDictionary.ButtonStudServices, Icon = "sitemap.png" }
                 };
             }
         }
-        
+
+        public MenuItem SelectedItem
+        {
+            get
+            {
+                return null;
+            }
+            set
+            {
+                CommandAction(value.Id);
+                RaisePropertyChanged();
+            }
+        }
+
         public void CommandAction(string actionName)
         {
             SimpleIoc.Default.GetInstance<Navigator>().HideMenu();
@@ -93,8 +106,6 @@ namespace PMF.ViewModels
         public string Text { get; set; }
 
         public string Icon { get; set; }
-
-        public Command Command { get; set; }
 
     }
 }
